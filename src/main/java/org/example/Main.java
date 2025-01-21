31e5da87251dda986778f9a076e5ce5371492fa0
Vale-n-tina <valentia.grg@gmail.com>
2025-01-21 16:29:47 +0100

    .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,6 @@ import jakarta.persistence.EntityManager;
 
 import java.time.LocalDate;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 	public static void main(String[] args) {
 		EntityManager entity = EntityManagerUtil.getEntityManager();
